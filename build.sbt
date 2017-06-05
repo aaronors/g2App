52ae29aa553578bf085dcdfac36c0549731b0c86
aaronors <pinoypinay93>
2017-06-05 16:42:27 -0700

    everything finished except for export and join

--- build.sbt
+++ build.sbt
@@ -5,4 +5,9 @@ version := "1.0"
 scalaVersion := "2.11"
 
 
-libraryDependencies += "org.apache.spark" % "spark-core_2.11"
+libraryDependencies ++= Seq("org.apache.spark" % "spark-core_2.11",
+                            "org.apache.spark" % "spark-sql_2.11"
+                            )
+
+
+
--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -4,69 +4,97 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql._
 
 
-
 /**
-  * Created by toddmcgrath on 6/15/16.
+  * Created by aaronors on 6/4/17.
   */
+
 object Solution {
 
   def main(args: Array[String]) {
 
 
-    /** Setup **/
+    /**----------------- Setup -----------------**/
 
-    val conf = new SparkConf().setAppName("dataAnalysis").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("Solution").setMaster("local[*]")
     val sc = new SparkContext(conf)
+
+    val spark = SparkSession
+      .builder()
+      .appName("SQLSolution")
+      .getOrCreate()
 
     System.setProperty("hadoop.home.dir", "C:\\winutils")
 
+    /**----------------- Assets -----------------**/
+
+   /** open assets.txt file **/
+    val assetsFile = sc.textFile("C:\\Users\\aaronors\\IdeaProjects\\g2App\\src\\main\\resources\\assets.txt") /** returns an RDD string **/
+
+    /** parse to RDD[string] of JSON lines **/
+    val assetsStr = assetsFile.map(line => parseLine(line))
 
 
-    /** open assets file **/
-    val textFile = scala.io.Source.fromURL("http://gumgum-share.s3.amazonaws.com/assets_2014-01-20_00_domU-12-31-39-01-A1-34.gz").mkString
+    /** convert lines to DF **/
+    val assetsDF = spark.read.json(assetsStr)
+    assetsDF.createOrReplaceTempView("assets")
 
-    val list = textFile.split("\n")
-
-
-    list.take(2).foreach(println)
-
+    /** access using SQL statements **/
+    val pageviewDF = spark.sql("SELECT DISTINCT pv, COUNT(pv) FROM assets GROUP BY pv ")
 
 
+    /** output for pageviewDF100.txt **/
+    //pageviewDF.take(100).foreach(println)
 
-    /** split each line with comma **/
-
-
-
-
-
-    /** create new file, parse assets file, output into new file   **/
+    /**----------------- Ad Events -----------------**/
 
 
+    val adeventFile = sc.textFile("C:\\Users\\aaronors\\IdeaProjects\\g2App\\src\\main\\resources\\ad_events.txt") /** returns an RDD string **/
 
-    /**----------- Proposed Solution ------------------
-      *  - open file(do for both)
-      *  - extract relevant json
-      *  - convert to json line file
-      *---------------------------------------------**/
+    val adeventStr = adeventFile.map(line => parseLine(line))
+
+    val adeventDF = spark.read.json(adeventStr)
+
+    adeventDF.createOrReplaceTempView("adevent")
+
+    /** one sql for views and one for clicks **/
+
+    val eclickDF = spark.sql("SELECT DISTINCT pv, COUNT(pv) AS clickCnt FROM adevent WHERE e LIKE 'click' GROUP BY pv")
+
+    /** output for eclickDF100.txt **/
+   // eclickDF.take(100).foreach(println)
+
+
+    val eviewDF = spark.sql("SELECT DISTINCT pv, COUNT(pv) AS viewCnt FROM adevent WHERE e LIKE 'view' GROUP BY pv")
+
+    /** output for eviewDF100.txt **/
+    // eviewDF.take(100).foreach(println)
+
+
+    /**----------- Formatting and Output -----------**/
+
+    /** Join outputs by pv**/
+
+   /** val output = adeventDF.join(eclickDF, adeventDF("pv") === eclickDF("pv"))
+      .join(eviewDF, adeventDF("pv") === eviewDF("pv"))
+      .select(adeventDF("pv"),eclickDF("clickCnt"),eviewDF("viewCnt")) **/
+
+
+    /** Output to text file **/
+
+
+   /** val out = pageviewDF.write.save("C:\\Users\\aaronors\\IdeaProjects\\g2App\\src\\main\\output\\out1.txt") **/
+
+
+
   }
 
+  /** split each line and return the last JSON**/
+  def parseLine(str : String): String ={
+    val strParse = str.split("\\, \\{")
+    val bracket = "{"
+
+    return bracket.concat(strParse(strParse.length-1))
+  }
 }
 
 
-/** --------------------- Notes ----------------------
-  *
-  * (make sure to check documentation for correct version !!!!!!!!!)
-  *
-  * sql context not used in 2.1.1
-  *
-  *
-  *
-  *
-  *
-  *
-  *
-  *------------------------------------------------**/
-
-
-
-/** **/
